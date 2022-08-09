629c3c2611984f0d2fe05158554b956bbb0ae59c
Kishor Davara <kishordavara.patel49@gmail.com>
2022-08-09 16:15:10 +0530

    KAFKA: producer - sticky and round robin partition demo

--- kafka-basics/src/main/java/org/kafka/demo/ProducerDemoWithCallBack.java
+++ kafka-basics/src/main/java/org/kafka/demo/ProducerDemoWithCallBack.java
@@ -1,6 +1,7 @@
 package org.kafka.demo;
 
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,37 +10,47 @@ import java.util.Properties;
 
 public class ProducerDemoWithCallBack {
     private static final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallBack.class.getSimpleName());
+
     public static void main(String[] args) {
         logger.info("::Kafka Producer::");
         //create producer properties
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         //create the producer
-        KafkaProducer<String ,String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create a record
-        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","Hello World");
+        for (int count = 1; count <= 10; count++) {
+            // create a record
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello World "+ count);
 
-        //send the data - asynchronous operation
-        producer.send(producerRecord, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                //executes every time a record is successfully sent or an exception is thrown
-                if(exception == null) {
-                    // the record was successfully sent
-                    logger.info("Received new metadata/n \n" +
-                            "Topic: " + metadata.topic() + "\n" +
-                            "Partition: " + metadata.partition() + "\n" +
-                            "Offset: " + metadata.offset() + "\n" +
-                            "Timestamp: " + metadata.timestamp());
-                } else {
-                    logger.error("Error while producing the record ", exception);
+            //send the data - asynchronous operation
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    //executes every time a record is successfully sent or an exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata/n \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        logger.error("Error while producing the record ", exception);
+                    }
                 }
-            }
-        });
+            }); // uses the sticky partitioner as default partitioner to improve performance
+
+           //just to check the round robin partition demo add below code
+//           try {
+//               Thread.sleep(1000);
+//           } catch (InterruptedException e) {
+//               e.printStackTrace();
+//           }
+        }
 
         //flush data - synchronous operation and close the producer
         //producer.flush(); // flush the data
